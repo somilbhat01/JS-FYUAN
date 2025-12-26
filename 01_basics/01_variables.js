5ff1936ba49fc8ad688d2b0f4a1e5ade7ab98f98
somilbhat01 <bhatlucky163@gmail.com>
2025-12-26 08:56:34 +0000

    Add basics exercises

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 123456
+let accountEmail = "Somilbhat1115@ gmail.com"
+var accountPassword = "Somil@1234"
+accountCity = "jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "ji43@k.com"
+accountPassword = "nej vfdjs"
+accountCity = "delhi"
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, accountCity]);  
+
+/*
+prefer not to use var now a days
+because of its function scope
+use let and const
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("") we are using nodejs, not browser
+
+//console.log("Hello World")
+
+let name = "Somil"
+let age = 19
+let isoggedIn = true
+
+//number =>2 to power 53 
+//bigint
+//string =>"" or ''
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
+//object 
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object (bug)
