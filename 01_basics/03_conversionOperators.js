e2a7c112126f8132024619aaa86dc85523c5f15c
somilbhat01 <bhatlucky163@gmail.com>
2025-12-26 11:38:20 +0000

    Add conversion operators exercise

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,19 @@
+let score = 33
+
+//"33" => 33 
+// "33abc" => NaN 
+// true => 1 ; false => 0 
+let isLoggedIn = 1 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false 
+    // empty string "" boolean => false 
+    // if any other thing is used => true 
+// other thing(somil in boolean) => true
+
+let someNumber = 36
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+// we converted number into string 
