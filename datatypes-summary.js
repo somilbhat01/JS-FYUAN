6c9bab7646c1f7bee1847be725e7358581c04be8
somilbhat01 <bhatlucky163@gmail.com>
2025-12-27 05:45:11 +0000

    data summary

---
+++ datatypes-summary.js
@@ -0,0 +1,9 @@
+// Primitive 
+
+// 7 Types : string , Number , Boolean ,null , undefined ,Symbol -> (to make a thing unique), BigInt 
+
+const score = 100 
+
+// Reference ( Non primitive )
+
+// Array, Objects , Functions 
