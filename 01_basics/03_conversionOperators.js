43465f3c15e26d14d0ececca608dd2fc09a0b402
somilbhat01 <bhatlucky163@gmail.com>
2025-12-27 05:02:21 +0000

    Updated string concatenation logic

--- 01_basics/03_conversionOperators.js
+++ 01_basics/03_conversionOperators.js
@@ -5,7 +5,7 @@ let score = 33
 // true => 1 ; false => 0 
 let isLoggedIn = 1 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false 
     // empty string "" boolean => false 
@@ -14,6 +14,33 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 36
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 // we converted number into string 
+
+let value = 3 
+let negValue = -value
+//console.log(negValue);
+// we converted 3 into -3
+
+//console.log(2+2); //4
+//console.log("2"+"2"); //"22"
+//console.log("2"+2); //"22"
+//console.log(2+"2"); //"22"
+//console.log(2-2); //0
+//console.log(2*2); //4
+//console.log(2/2); //1
+//console.log(2**2); //4
+//console.log(2%2); //0
+
+//let str1 = "Somil"
+//let str2 = " Bhat"
+
+//let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1"+2+2); //"122"
+//console.log(2+2+"1"); //"41"
+
+//console.log(+true); //1
+//console.log(+""); //0
