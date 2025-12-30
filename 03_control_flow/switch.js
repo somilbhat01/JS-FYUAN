ef79fcc71ade2ed5f7cc617274ffeef7c18834d3
somilbhat01 <bhatlucky163@gmail.com>
2025-12-30 09:34:57 +0000

    switch.

---
+++ 03_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;             // if you didn't used break than the rest of the statement below will be executed except for default 
+
+    default:
+        console.log("default case match");
+        break;
+}
